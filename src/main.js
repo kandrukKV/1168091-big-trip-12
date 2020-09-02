c376819de5778ca4647d8a30d07424d804bd419b
Konstantin Kandruk <kandruk@ya.ru>
2020-09-02 11:34:28 +0300

    module6-task2 v1

--- src/main.js
+++ src/main.js
@@ -11,8 +11,6 @@ const data = getData();
 
 const {events} = data;
 
-console.log(events[0]);
-
 const siteTripMainElement = document.querySelector(`.trip-main`);
 const siteTripControlsElement = siteTripMainElement.querySelector(`.trip-main__trip-controls`);
 const siteMenuElement = siteTripMainElement.querySelector(`h2`);
